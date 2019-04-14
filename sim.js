9f1ef32d9a61c6b90bfcce68829be6f84c0bb3db
Salomon Honkala <salomon.j.honkala@utu.fi>
2019-04-14 11:24:11 +0300

    Fix creature polymorhism

--- sim.js
+++ sim.js
@@ -1,47 +1,43 @@
-'use strict'
+"use strict";
 
 class World {
   constructor(w, h, creatures) {
-    this.width = w
-    this.height = h
-    this.time = 0
-    this.creatures = (creatures ? creatures : [])
+    this.width = w;
+    this.height = h;
+    this.time = 0;
+    this.creatures = creatures ? creatures : [];
   }
   progress() {
-    this.time++
+    this.time++;
   }
   addCreature(creature) {
-    this.creatures.push(creature)
+    this.creatures.push(creature);
   }
 }
 
 class Creature {
   constructor(x, y, type) {
-    this.x = x
-    this.y = y
-    this.type = type
+    this.x = x;
+    this.y = y;
+    this.type = type;
     this.hunger = true;
   }
-  eat() {this.hunger = false};
+  eat() {
+    this.hunger = false;
+  }
 }
 
 class Plant extends Creature {
-  constructor() {
-    super()
-  }
+  
 }
 
 class Animal extends Creature {
-  constructor() {
-    super()
-  }
+
   move() {
-     let {x, y} = randomWalk(this.x, this.y)
-     this.x = x
-     this.y = y
-   }
+    let { x, y } = randomWalk(this.x, this.y);
+    this.x = x;
+    this.y = y;
+  }
 }
 
-function randomWalk(x, y) {
-
-}
+function randomWalk(x, y) {}
